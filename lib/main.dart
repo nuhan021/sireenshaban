0bcfa4f47217c7d237c27692e59193af785e87ce
MD Ali Nuhan Chowdhury <nuhan@Nuhans-MacBook-Pro.local>
2025-12-20 23:49:55 +0600

    Add: stripe api

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:intl/date_symbol_data_file.dart';
 import 'package:sireenshaban/app.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,7 @@ void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
   // await initializeDateFormatting('en_US', "");
+  Stripe.publishableKey = "pk_test_51RTEbLFT92q9uNcDVhh5ojhH4AaEanWSPbbTgWCOKjBUnsmhccT5bYt3nKWcm3etcmKrZ6kSP4cjFGMqSoFRHRzP00WCjN0XbD";
   await dotenv.load(fileName: ".env");
   await StorageService.init();
 
